95a91d08ba3d82364fe8e9f7afd0096e207672d8
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-29 17:44:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// write code here
+function findIndex(array) {
+  let arr = []
+  for(i = 0 ; i < array.length ; i++){
+   if(array[i] ==`a`){
+     arr.push(i)
+  }
+}
+  return arr
+}
+const alphabets = ['a', 'b', 'a', 'c', 'a', 'd'];
+// expexted result: [0, 2, 4]
+findIndex(alphabets)
